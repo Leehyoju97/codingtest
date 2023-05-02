a6d59d0e2c8b6fab42b50d716e5722dcb39bdf82
Leehyoju97 <dlgywn2420@gmail.com>
2023-05-02 22:17:25 +0900

    feat : 백준 스택

--- src/baekjoon/datastructure/Main10828.java
+++ src/baekjoon/datastructure/Main10828.java
@@ -1,29 +1,30 @@
 package baekjoon.datastructure;
 
-import java.util.Scanner;
+import java.io.*;
 import java.util.Stack;
 
 public class Main10828 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < n; i++) {
-            String cmd = sc.next();
+            String[] str = br.readLine().split(" ");
 
-            if (cmd.equals("push")) {
-                int num = sc.nextInt();
+            if (str[0].equals("push")) {
+                int num = Integer.parseInt(str[1]);
                 stack.push(num);
-            } else if (cmd.equals("top")) {
+            } else if (str[0].equals("top")) {
                 System.out.println((stack.isEmpty()) ? -1 : stack.peek());
-            } else if (cmd.equals("pop")) {
+            } else if (str[0].equals("pop")) {
                 System.out.println((stack.isEmpty()) ? -1 : stack.pop());
-            } else if (cmd.equals("size")) {
+            } else if (str[0].equals("size")) {
                 System.out.println(stack.size());
-            } else if (cmd.equals("empty")) {
+            } else if (str[0].equals("empty")) {
                 System.out.println((stack.isEmpty()) ? 1 : 0);
             }
         }
