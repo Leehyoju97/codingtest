6c8162c56e52ea14ea70402a6b4358ffc8484289
Leehyoju97 <dlgywn2420@gmail.com>
2023-12-10 19:43:44 +0900

    백준 숫자카드2

--- src/baekjoon/Main10815.java
+++ src/baekjoon/Main10815.java
@@ -4,10 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main10815 {
-    public static void main(String[] args) throws IOException {
+    /*public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] card = new int[n];
@@ -27,10 +28,39 @@ public class Main10815 {
         st = new StringTokenizer(br.readLine(), " ");
 
         for (int i = 0; i < m; i++) {
+            // 0보다 크면 해당 값이 존재
             if (Arrays.binarySearch(card, Integer.parseInt(st.nextToken())) >= 0) {
                 sb.append(1).append(' ');
             } else {
                 sb.append(0).append(' ');
+            }
+        }
+
+        System.out.println(sb);
+    }*/
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        HashMap<String, Integer> owned = new HashMap<>();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < n; i++) {
+            owned.put(st.nextToken(), 0);
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < m; i++) {
+            if (owned.get(st.nextToken()) != null) {
+                sb.append(1).append(" ");
+            } else {
+                sb.append(0).append(" ");
             }
         }
 
